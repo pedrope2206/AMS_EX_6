9886c7f6581637bcdb630247cbd2a33ed0e04d90
pedrope2206 <pivettapedro93@gmail.com>
2022-05-27 16:59:03 -0300

    AMS_EX_6

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿string nome = "";
+double H = 0;
+double peso = 0;
+
+Console.WriteLine($"Olá, qual é o seu nome?");
+nome = Console.ReadLine();
+
+Console.WriteLine("");
+
+Console.WriteLine($"Olá, {nome}, vamos calcular seu peso ideal");
+
+Console.WriteLine("");
+
+Console.WriteLine("Digite a sua altura abaixo");
+H = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("");
+
+peso = (72.7 * H) -58;
+
+Console.WriteLine($"{nome}, seu peso ideal é de: {peso}");
